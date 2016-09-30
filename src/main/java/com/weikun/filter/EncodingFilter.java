76c91dc2825714f304c0f5baa6054055cbba7b02
weikun <wk2003119@163.com>
2016-09-30 19:55:53 +0800

    9301

--- src/main/java/com/weikun/filter/EncodingFilter.java
+++ src/main/java/com/weikun/filter/EncodingFilter.java
@@ -13,7 +13,11 @@ public class EncodingFilter implements javax.servlet.Filter {
     public void doFilter(javax.servlet.ServletRequest req, javax.servlet.ServletResponse resp, javax.servlet.FilterChain chain) throws javax.servlet.ServletException, java.io.IOException {
 
         req.setCharacterEncoding(encoder);
+
+
+
         chain.doFilter(req, resp);
+
     }
 
     public void init(javax.servlet.FilterConfig config) throws javax.servlet.ServletException {
